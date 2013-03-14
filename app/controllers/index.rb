fbf7726cac3644e416b4d12c201c4daf47ebf980
Apprentice <apprentice@devbootcamp.com>
2013-03-13 19:35:20 -0700

    Completed merger of Bitly and Userauth

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,9 +3,42 @@ get '/' do
 end
 
 post '/urls' do
-  @errors = Url.create.errors[:original_url].any?
-  @url = Url.create(params).invalid?
+  # @url_error = Url.create.errors[:original_url].any?
+  @url_error = Url.create(params).invalid?
+  @url = Url.create(params)
   erb :urls
+end
+
+get '/user/new' do
+  if session.has_key?(:user)
+    erb :secret_page
+  else
+    redirect '/'
+  end
+end
+
+post '/user/new' do
+  @current_user = User.create(params[:user])
+  erb :secret_page
+end
+
+get '/logout' do
+  logout
+  redirect '/'
+end
+
+get '/authenticate' do
+  @inputted_email = params[:email]
+  @inputted_password = params[:password]
+
+  user = User.authenticate(@inputted_email, @inputted_password, session)
+  if user
+    redirect '/user/new'
+  else
+    @error = true
+    erb :index
+    #redirect '/'
+  end
 end
 
 get '/:short_url' do
@@ -14,4 +47,3 @@ get '/:short_url' do
   url_object.save
   redirect url_object.original_url
 end
-
--- app/models/url.rb
+++ app/models/url.rb
@@ -2,7 +2,6 @@ class Url < ActiveRecord::Base
   validates :original_url, :format => { :with => /^(http|https|ftp)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(:[a-zA-Z0-9]*)?\/?([a-zA-Z0-9\-\._\?\,\'\/\\\+&amp;%\$#\=~])*[^\.\,\)\(\s]$/
 }
 
-
   before_create :create_shortened_url
   before_save :increment_counter
 
